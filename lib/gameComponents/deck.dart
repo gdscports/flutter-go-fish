0a19fff1e2d29e667d4e9441e50d69ba012e240d
Johnny Morrison-Howe <50030862+Dexyboiiii@users.noreply.github.com>
2022-11-23 17:57:51 +0000

    Added deck class

--- lib/gameComponents/deck.dart
+++ lib/gameComponents/deck.dart
@@ -1,1 +1,30 @@
-class Deck {}
+import 'card.dart';
+import 'hand.dart';
+
+class Deck {
+  List<Hand> hands = [];
+  List<Card> cards = [];
+
+  Deck() {
+    for (var suit in Suit.values) {
+      for (var rank in ranks.entries) {
+        cards.add(
+          Card(
+            suit,
+            rank.key,
+            'cards/${suit.name}s/card_${suit.name}s_${rank.key}.png',
+          ),
+        );
+      }
+    }
+    cards.shuffle();
+  }
+
+  void shuffle() {
+    cards.shuffle();
+  }
+
+  void shuffleHand(int hand) {
+    hands[hand].shuffle();
+  }
+}
